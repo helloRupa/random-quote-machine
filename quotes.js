a468401f664ba0112e4e41a24c39bae468cdd286
Rupa Dhillon <rupa211984@gmail.com>
2018-04-02 21:48:05 -0700

    Replace var w/ let, change function names for getData and makeScript, and more

--- quotes.js
+++ quotes.js
@@ -1,54 +1,63 @@
-getData_42190abhy = function (response) {
-    var quote = document.getElementById("quotes").getElementsByTagName("p")[0];
-    var author = document.getElementById("quotes").getElementsByTagName("cite")[0];
-    try {
-        if (!response) throw "New exception";
-        var authorText = response.quoteAuthor;
-        var quoteText = response.quoteText;
-        quote.textContent = quoteText;
-        if (authorText) author.textContent = authorText;
-        else author.textContent = "Unknown";
-    } catch (err) {
-        quote.textContent = "Sorry, quotes are not currently available. Please check back later.";
-        author.textContent = "Sad Malfunction Bear";
-    }
-};
-
 (function () {
     window.onload = (function () {
+        let url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp";
         getSetHeight();
-        makeQuoteScript();
+        jsonP(url, "handleRes", showQuote);
         fadeIn();
         hTweet(1000);
         document.getElementById("getNewQuote").onclick = function () {
             getSetHeight();
             fadeOut();
             setTimeout(function () {
-                makeQuoteScript();
+                jsonP(url, "handleRes", showQuote);
             }, 1000);
             fadeIn();
             hTweet(2100);
         };
     })();
 
-    function makeQuoteScript() {
-        var script = document.createElement("script");
-        script.id = "quoteScript";
-        script.type = "text/javascript";
-        script.src = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=getData_42190abhy";
-        script.onerror = function (event) {
-            document.getElementById("quotes").getElementsByTagName("p")[0].textContent = "Sorry, quotes are not currently available. Please check back later.";
-            document.getElementById("quotes").getElementsByTagName("cite")[0].textContent ="Sad Malfunction Squatch";
+    function showError(err) {
+        let quote = document.getElementById("quotes").getElementsByTagName("p")[0];
+        let author = document.getElementById("quotes").getElementsByTagName("cite")[0];
+        quote.textContent = "Sorry, quotes are not currently available. Please check back later.";
+        author.textContent = "Sad Malfunction Bear";
+        console.log(err);
+    }
+
+    function showQuote(response) {
+        let quote = document.getElementById("quotes").getElementsByTagName("p")[0];
+        let author = document.getElementById("quotes").getElementsByTagName("cite")[0];
+        let quoteText = response.quoteText.trim();
+        let authorText = response.quoteAuthor.trim();
+        quote.textContent = quoteText;
+        if (authorText) author.textContent = authorText;
+        else author.textContent = "Unknown";
+    }
+
+    function jsonP(url, callbackName, callbackFn) {
+        let script = document.createElement("script");
+        script.src = `${url}&jsonp=${callbackName}`;
+        window[callbackName] = function(response) {
+            try {
+                if (!response) throw "New exception";
+                if (typeof callbackFn === "function") callbackFn(response);
+            } catch (err) {
+                showError(err);
+            }
+        }; 
+        script.onerror = function (err) {
+            showError(err);
         };
         document.getElementsByTagName("head")[0].appendChild(script);
-        document.getElementById("quoteScript").remove();
+        script.remove();
     }
 
     function makeTweet() {
-        var tweetBtn = document.getElementById("tweetBtn");
-        var quote = document.getElementById("quotes").getElementsByTagName("p")[0].textContent;
-        var author = document.getElementById("quotes").getElementsByTagName("cite")[0].textContent;
-        tweetBtn.href = 'https://twitter.com/intent/tweet?text="' + quote + '"' + " -" + author;
+        let tweetBtn = document.getElementById("tweetBtn");
+        let quote = document.getElementById("quotes").getElementsByTagName("p")[0].textContent;
+        quote = quote.replace(/;/g, ",");
+        let author = document.getElementById("quotes").getElementsByTagName("cite")[0].textContent;
+        tweetBtn.href = `https://twitter.com/intent/tweet?text="${quote}" -${author}`;
     }
 
     function fadeOut() {
@@ -62,10 +71,10 @@ getData_42190abhy = function (response) {
     }
 
     function getSetHeight() {
-        var qcont = document.getElementById("quotes");
-        var qblock = document.getElementsByTagName("blockquote")[0];
-        var ht = qblock.scrollHeight;
-        qcont.style.height = ht + "px";
+        let qCont = document.getElementById("quotes");
+        let qBlock = document.getElementsByTagName("blockquote")[0];
+        let ht = qBlock.scrollHeight;
+        qCont.style.height = ht + "px";
     }
 
     function hTweet(time) {
